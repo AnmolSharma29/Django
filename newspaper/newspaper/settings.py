0e2533379267e8b78dd96a3fe5f6cd94cbd84624
Anmol Sharma <anmolsharma2902@gmail.com>
2024-07-16 15:53:08 +0530

    postgres apply

--- newspaper/newspaper/settings.py
+++ newspaper/newspaper/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'newspaper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'dlGHAEQdtHnpbkRHQbCPRwfoojarCcvX',
-#         'HOST': 'monorail.proxy.rlwy.net',
-#         'PORT': '41199',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'dlGHAEQdtHnpbkRHQbCPRwfoojarCcvX',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '41199',
+    }
+}
 
 
 # Password validation
